4480f976c39e27cc4a9106981590e020ddeedec9
Tamanna <ttamanna.202022@gmail.com>
2026-03-04 15:00:07 +0600

    customer file added

---
+++ Customer.java
@@ -0,0 +1,28 @@
+import java.io.Serializable;
+
+public class Customer implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private int customerId;
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+    
+    public Customer(int customerId, String name, String email, String phone, String address) {
+        this.customerId = customerId;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+    }
+    
+    public int getCustomerId() { return customerId; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
+}
